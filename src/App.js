0e15999746044d12d3f62c3055a89d7e0f11129c
AmEhlee <ElieKabengele.Work@gmail.com>
2023-04-17 23:43:02 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center">
+      <header className="App-header">
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
