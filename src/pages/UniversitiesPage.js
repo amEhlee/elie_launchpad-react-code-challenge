e367f144e68bd77049048fe72aa769aeaf900ed5
AmEhlee <ElieKabengele.Work@gmail.com>
2023-04-19 15:42:38 -0600

    Question 3 Complete - add useeffect that checks for change in country - now renders a list of univiersites based on given country - only included basic details and not full json as a style choice

--- src/pages/UniversitiesPage.js
+++ src/pages/UniversitiesPage.js
@@ -16,6 +16,7 @@ import axios from "axios";
 export default function UniversitiesPage() {
   const [country, setCountry] = React.useState("");
   const [countryList, setCountryList] = useState([]);
+  const [entities, setEntities] = useState([]);
 
   useEffect(() => {
     const countryListSource =
@@ -30,6 +31,21 @@ export default function UniversitiesPage() {
         console.log(err);
       });
   }, []);
+
+  useEffect(() => {
+    if (country !== "") {
+      const getUniSource = `http://universities.hipolabs.com/search?country=${country}`;
+
+      axios
+        .get(getUniSource)
+        .then((res) => {
+          setEntities(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [country]);
 
   return (
     <div className="mt-20 mx-8 text-center ">
@@ -51,6 +67,27 @@ export default function UniversitiesPage() {
             ))}
           </Select>
         </FormControl>
+
+        <Table className="min-w-[800px]">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Country</TableCell>
+              <TableCell align="center">State / Province</TableCell>
+              <TableCell align="center">Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {entities.map((i) => (
+              <TableRow key={i.name}>
+                <TableCell align="center">{i.country}</TableCell>
+                <TableCell align="center">
+                  {i["state-province"] || " None "}
+                </TableCell>
+                <TableCell align="center">{i.name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Card>
     </div>
   );
