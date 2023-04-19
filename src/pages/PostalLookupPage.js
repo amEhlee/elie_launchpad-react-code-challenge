2ea780d76b13b1882ba51038d573f8ba4839d36e
AmEhlee <ElieKabengele.Work@gmail.com>
2023-04-19 16:27:47 -0600

    Inital start Question 4 Postal Lookup - route and updated navbar - get request firing and logging to console

--- src/pages/PostalLookupPage.js
+++ src/pages/PostalLookupPage.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Card,
+  InputLabel,
+  FormControl,
+  TextField,
+  Button,
+} from "@mui/material";
+import axios from "axios";
 
 export default function PostalLookupPage() {
+  const [postalCode, setPostalCode] = useState("");
+  const [data, setData] = useState({});
+
+  function handlePostalLookup() {
+    const postalLookupApi = `https://api.zippopotam.us/us/${postalCode}`;
+
+    axios
+      .get(postalLookupApi)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
-    <div>PostalLookupPage</div>
-  )
+    <div className="mt-20 mx-8 text-center p-4">
+      <Card className="flex flex-col gap-4 mx-4 p-4">
+        <h1 className="my-8 mx-4">Postal Lookup</h1>
+        <p>Warning this may only work with US postal codes</p>
+        <FormControl>
+          <div className="flex justify-center gap-4">
+            <TextField
+              label="Postal Code"
+              onChange={(e) => {
+                setPostalCode(e.target.value);
+              }}
+            />
+
+            <Button onClick={handlePostalLookup} variant="outlined">
+              Search
+            </Button>
+          </div>
+        </FormControl>
+      </Card>
+    </div>
+  );
 }
